89197fb0dc0f07fd4be2bfee03ff70b253d0737b
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2014-08-28 11:30:30 -0300

    Update Provento.java

--- GerenteFinaceiroLP2/src/Projeto/LP2/Provento.java
+++ GerenteFinaceiroLP2/src/Projeto/LP2/Provento.java
@@ -4,12 +4,9 @@ import java.io.Serializable;
 
 public class Provento extends Transacao implements Serializable{
 	private static final long serialVersionUID = 1L;
-
 	public Provento(String descricao, double valor, Recorrencia rec, CategoriaDeTransacao categoria) throws Exception {
 		super(descricao,valor,rec,categoria);
 		geraCodigo("P");
-		
 	}
-
 }
 
